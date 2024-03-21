161969e65b7f4675047f4880e81b6565bae309f5
melkisedek111 <melkisedek222@gmail.com>
2024-03-22 06:17:21 +0800

    Adding migration files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'drizzle-kit'
+
 export default defineConfig({
  schema: "./src/db/schema.ts",
+ out: "./lib/db/migrations",
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
